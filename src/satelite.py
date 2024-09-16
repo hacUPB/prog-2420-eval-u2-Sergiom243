444ebf925ffacc7acec57bf1385a4a5e4edee07e
Sergiom243 <Sergio.marinp@upb.edu.co>
2024-09-16 00:03:20 -0500

    Satelite finalmente

--- src/satelite.py
+++ src/satelite.py
@@ -1,25 +1,33 @@
 orbita = 0
+incremento_arrastre = 0
 
-Altitud_inicial = float(input("por favor ingrese una altitud inicial en km: "))
-Altitud_mínima = float(input("por favor ingrese una altitud de seguridad en km: "))
-Coeficiente_arrastre = float(input("por favor ingrese un coeficiente de arrastre(ejemplo: 0.01): "))
+verificacion_altitud = True
+while verificacion_altitud:
+    Altitud_inicial = float(input("Por favor ingrese una altitud inicial en km: "))
+    Altitud_mínima = float(input("Por favor ingrese una altitud de seguridad en km: "))
 
-final_simulacion = True 
+    if Altitud_inicial <= Altitud_mínima:
+        print("Error: La altitud inicial debe ser mayor que la altitud mínima. Intente de nuevo.")
+    else:
+        verificacion_altitud = False 
+
+Coeficiente_arrastre = float(input("Por favor ingrese un coeficiente de arrastre (ejemplo: 0.01): "))
+incremento_arrastre += 0.00001   
+
+final_simulacion = True
 while final_simulacion:
-
     cambio_altitud = Altitud_inicial * Coeficiente_arrastre
     Diferencia_altitud = Altitud_inicial - cambio_altitud
+    Altitud_inicial = Diferencia_altitud
     orbita += 1
-    Coeficiente_arrastre += 0.0001 
 
     if Diferencia_altitud < Altitud_mínima:
-        print (f"el satelite regreso a la órbita y se desintegró, logro {orbita} orbitas")
+        print(f"El satélite regresó a la atmósfera y se desintegró. Logró {orbita} órbitas.")
         final_simulacion = False
 
-    if Diferencia_altitud < :
-        print (f"el satelite se estabilizó a las {orbita} orbitas")
+    elif cambio_altitud < 0.01:
+        print(f"El satélite se estabilizó a las {orbita} órbitas.")
         final_simulacion = False
 
-
-
-    print (f"Orbita: {orbita} altitud en km: {Diferencia_altitud} y coeficiente de arrastre: {Coeficiente_arrastre}")
+    else:
+        print(f"Órbita: {orbita} - Altitud en km: {Diferencia_altitud:.2f} - Coeficiente de arrastre: {Coeficiente_arrastre:.4f}")
